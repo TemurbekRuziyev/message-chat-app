4343982821630a7c37565cdc2f986a9795c71bc6
Temurbek Ruziyev <bestcoder0521@gmail.com>
2023-04-15 11:56:52 +0500

    fixes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,35 +1,20 @@
-import React, { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
+import React, { useEffect } from 'react';
+import { io } from 'socket.io-client';
+
+import './App.css';
+
+const socket = io('http://127.0.0.1:8080/');
 
 function App() {
-  const [count, setCount] = useState(0);
+  useEffect(() => {
+    socket.on('connect', () => {
+      socket.on('welcome', message => {
+        console.log(message);
+      });
+    });
+  }, []);
 
-  return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank" rel="noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount(count => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  );
+  return <div className="App">Welcome to the chat app</div>;
 }
 
 export default App;
